b50e1618121fc664c3dc8d8eefa5a3f2a69686c6
Marisol Perez <marii.jpg123@gmail.com>
2025-07-15 21:02:37 -0500

    Card color updated

--- atomic_design_system/lib/organisms/product_card.dart
+++ atomic_design_system/lib/organisms/product_card.dart
@@ -26,6 +26,7 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.all(12),
         child: Column(
